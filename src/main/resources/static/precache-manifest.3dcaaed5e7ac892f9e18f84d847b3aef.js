9b2d1aa28377289ffea683d7fd8d23ed49112396
Sung Lee <sungw.lee1@gmail.com>
2021-12-13 00:02:27 -0500

    Add React to Heroku

---
+++ src/main/resources/static/precache-manifest.3dcaaed5e7ac892f9e18f84d847b3aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1349d03c23dbe1c7b8228a60d198f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2898141aa081286d4328",
+    "url": "/static/css/main.5d560caf.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "2898141aa081286d4328",
+    "url": "/static/js/main.0d49a66c.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
